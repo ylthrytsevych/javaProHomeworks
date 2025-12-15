0339112cfaa03d8fe69fcef9eb32a32b35a75a85
hrytsevychyt <hrytsevychyt@dtek.com>
2025-12-15 15:57:38 +0200

    hw1 step 14, push to master

--- homeworks/hw1/app/Main.java
+++ homeworks/hw1/app/Main.java
@@ -6,7 +6,7 @@ class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter app");
-
+        System.out.println("App for measures conversions.");
         double kgs = 5;
         double pounds = convKgsToPounds(kgs);
         System.out.printf("Result is %.2f pounds.%n", pounds);
