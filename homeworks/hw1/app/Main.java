64af0d7743028ed08ce32773085846aa1b0bd0d6
hrytsevychyt <hrytsevychyt@dtek.com>
2026-01-08 14:02:07 +0200

    Added hw2 from another repo

--- homeworks/hw1/app/Main.java
+++ homeworks/hw1/app/Main.java
@@ -1,4 +1,4 @@
-package app;
+package hw1.app;
 
 class Main {
 
---
+++ homeworks/hw2/app/Main.java
@@ -0,0 +1,25 @@
+package hw2.app;
+
+class Main {
+
+    public static final double CONV_M_TO_KM = 1.60934;
+
+    public static void main(String[] args) {
+        System.out.println("Converter app");
+
+        double miles = 5;
+        double kilms = 10;
+        double KMS = convMilesToKms(miles);
+        double MILS = convKmsToMiles(kilms);
+        System.out.printf("Result are: %n%.0f miles is %.2f kilometers.%n%.0f kilometers is %.2f miles%n",
+                miles, KMS, kilms, MILS);
+    }
+
+    private static double convMilesToKms(double mils) {
+        return mils * CONV_M_TO_KM;
+    }
+
+    private static double convKmsToMiles(double kms) {
+        return kms / CONV_M_TO_KM;
+    }
+}
