76e402c49bf390e3d7ac948499afef406a0a90d2
hrytsevychyt <hrytsevychyt@dtek.com>
2026-01-08 15:26:56 +0200

    hw3, added converter, steps 6-9

--- homeworks/hw3/app/Main.java
+++ homeworks/hw3/app/Main.java
@@ -3,7 +3,15 @@ package hw3.app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Temperature converter app");
-        System.out.println("Version 1.0");
+        System.out.println("Version 1.2");
+        System.out.println("----------");
+
+
+        double tempF = 98;  //98 far = 36.6 cel
+        double celsius = Converter.convertFahrenheitToCelsius(tempF);
+
+        System.out.printf("%.1f degrees Fahrenheit is %.1f degrees Celsius.%n",
+                tempF, celsius);
 
     }
 }
