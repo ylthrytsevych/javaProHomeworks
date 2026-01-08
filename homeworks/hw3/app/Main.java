191cad1fb80a1b47fb037fd18c7d6bc45c52af4b
hrytsevychyt <hrytsevychyt@dtek.com>
2026-01-08 15:17:30 +0200

    hw3, step 1-5

---
+++ homeworks/hw3/app/Main.java
@@ -0,0 +1,9 @@
+package hw3.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Temperature converter app");
+        System.out.println("Version 1.0");
+
+    }
+}
