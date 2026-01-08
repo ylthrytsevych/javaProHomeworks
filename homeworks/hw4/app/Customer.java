232799313a4d56f5db36e202ce0722de2d0348bb
hrytsevychyt <hrytsevychyt@dtek.com>
2026-01-08 18:03:03 +0200

    hw4

---
+++ homeworks/hw4/app/Customer.java
@@ -0,0 +1,24 @@
+package hw4.app;
+
+public class Customer {
+
+    // Виправте декларацію змінних класу
+    private final String name;
+    private final String phone;
+
+    // Виправте визначення конструктора
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+
+}
